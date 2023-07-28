7f1bf6139ceeaa3076b042349edcf24a146ace32
Siyam Zaman <siyamzamanshishir@gmail.com>
2023-07-28 10:39:22 +0600

    All files and config are added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        daisyui: {
+            themes: ["light", "dark", "cupcake"],
+          },
+    },
+    plugins: [],
+  }
